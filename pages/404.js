2df08fdd096612d8ec2464b6eb6d68f9e02cc08d
moura5412 <112732358+moura5412@users.noreply.github.com>
2022-09-21 14:14:52 -0300

    Add files via upload

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function Page404(){
+    return (
+        <div>
+            <h1>Voçê se perderu e caiu na pagina 404 :0</h1>
+            <Link href="/">Ir para a Home</Link>
+      </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import HomeScreen from "../src/Screens/HomeScreen/Index";
+
+export default HomeScreen;
+
+/*
+import React from "react";
+import Link from "../src/components/Link";
+import Footer from "../src/components/Patterns/Footer/Index";
+
+function Title({ children, as }){
+const Tag = as;
+
+  return(
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+}
+
+export default function Home() {
+  return (
+    <div>
+      <Title as="h1">Alura Cases - Home page</Title>
+      <Link href="/faq">Ir para a pagina do FAQ</Link>
+      <Footer />
+    </div>
+  )
+}
+*/
